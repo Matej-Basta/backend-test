f4cb124c2df6880062a41c7a6880f07b9361c3a6
matej <matej.basta@email.cz>
2023-01-09 18:22:12 +0100

    1st version of tasks

--- src/Service/ExchangeRateService.php
+++ src/Service/ExchangeRateService.php
@@ -12,6 +12,17 @@ class ExchangeRateService {
      * @return array<string>
      */
     public function getSupportedCurrencies(): array {
+
+        $data = $this->fetchData();
+
+        $data_keys = [];
+
+        foreach($data['symbols'] as $key => $value) {
+            array_push($data_keys, $key);
+        }
+
+        return $data_keys;
+
     }
 
     /**
@@ -23,5 +34,55 @@ class ExchangeRateService {
      * @return int
      */
     public function getExchangeAmount(int $amount, string $fromCurrency, string $toCurrency): int {
+
+        $arguments = [
+            "amount" => $amount,
+            "from" => $fromCurrency,
+            "to" => $toCurrency,
+        ];
+        
+        $data = $this->fetchData($arguments);
+
+        $result = round($data['result']);
+
+        echo $result;
+
+        return $result;
+
+    }
+
+    private function fetchData(array $parameters = []): array {
+        $curl = curl_init();
+
+        $url = "";
+
+        if (empty($parameters)) {
+            $url = "https://api.apilayer.com/exchangerates_data/symbols";
+        } else {
+            $url = "https://api.apilayer.com/exchangerates_data/convert?to={$parameters["to"]}&from={$parameters["from"]}&amount={$parameters["amount"]}";
+        }
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_HTTPHEADER => array(
+                "Content-Type: text/plain",
+                "apikey: kigiQGomtNhg3zsoWOb6LYcKRuhQp2fM"
+            ),
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET"
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        $data = json_decode($response, true);
+
+        return $data;
     }
 }
---
+++ src/index.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Opeepl\BackendTest\Service;
+
+require_once './Service/ExchangeRateService.php';
+
+$exchange_rate_service = new ExchangeRateService();
+
+$exchange_rate_service->getExchangeAmount(100, 'EUR', 'DKK');
